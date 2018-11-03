6e2781bb79bf0006290d3020f3c6494c959e5297
mike_neck <jkrt3333@gmail.com>
2018-11-04 01:02:55 +0900

    define API.

---
+++ Sources/SwiftRegex/SwiftRegex.swift
@@ -0,0 +1,54 @@
+import Foundation
+
+
+// Regexp before compile.
+public protocol SwiftRegexPattern {
+    func compile() -> SwiftRegex
+}
+
+// Swift implementation which every Swift Regex implements the api of.
+public protocol SwiftRegexImplementationRef {
+    func createPattern(of pattern: String) -> SwiftRegexPattern
+}
+
+struct NoImpl: SwiftRegexImplementationRef {
+    func createPattern(of pattern: String) -> SwiftRegexPattern {
+        fatalError("createPattern(pattern:) has not been implemented")
+    }
+}
+
+public struct SwiftRegexImplementation {
+
+    fileprivate var ref: SwiftRegexImplementationRef = NoImpl()
+
+    private init() {
+    }
+
+    public mutating func useImpl(of: SwiftRegexImplementationRef) {
+        self.ref = of
+    }
+
+    private static var __impl: SwiftRegexImplementation = SwiftRegexImplementation()
+
+    static public var by: SwiftRegexImplementation {
+        get {
+            return __impl
+        }
+    }
+}
+
+func pattern(of string: String) -> SwiftRegexPattern {
+    return SwiftRegexImplementation.by.ref.createPattern(of: string)
+}
+
+public protocol SwiftRegex: class {
+
+    var pattern: String { get }
+
+    func matcher(for targetString: String)
+}
+
+public protocol RegexMatcher {
+
+    var matches: Bool { get }
+}
